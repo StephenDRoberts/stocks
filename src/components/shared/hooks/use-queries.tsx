e95d9dba55d047b609622f1729df2390d4ee4428
Stephen Roberts <stephen.roberts.skybettingandgaming.com>
2020-08-28 14:53:02 +0100

    actually fixing axios this time *shame*

--- src/components/shared/hooks/use-queries.tsx
+++ src/components/shared/hooks/use-queries.tsx
@@ -1,19 +1,19 @@
-import { useState, useEffect } from 'react';
-import { DailyPrice, APIResponse } from '../types';
-import { get } from '../fetchers';
+import { useState, useEffect } from "react";
+import { DailyPrice, APIResponse } from "../types";
+import { get } from "../fetchers";
 
-export const useGetDailyPrices = async () => {
-    const initialData: DailyPrice[] = [];
-    const [data, setData] = useState<DailyPrice[]>(initialData);
+export const useGetDailyPrices = () => {
+  const initialData: DailyPrice[] = [];
+  const [data, setData] = useState<DailyPrice[]>(initialData);
 
-    const fn = async () => {
-        const { results } = await get<APIResponse>('./data/SHARE_HISTORY_DTG.json');
-        setData(results)
-    }
+  const fn = async () => {
+    const { results } = await get<APIResponse>("./data/SHARE_HISTORY_DTG.json");
+    setData(results);
+  };
 
-    useEffect(() => {
-        fn()
-    }, [])
+  useEffect(() => {
+    fn();
+  }, []);
 
-    return data;
-}
+  return data;
+};
